3674b5d0a9b59e21e425563e5d43f6f3e2089e65
kiamfestoh@gmail <kiamfestoh@gmail.com>
2015-05-22 15:34:15 +0300

    completed the login activity

--- ParseStarterProject/src/com/parse/starter/LoginActivity.java
+++ ParseStarterProject/src/com/parse/starter/LoginActivity.java
@@ -1,17 +1,33 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends Activity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login=(Button)findViewById(R.id.send);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, UserHouSearchActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        });
     }
 
     @Override
--- ParseStarterProject/src/com/parse/starter/SplashActivity.java
+++ ParseStarterProject/src/com/parse/starter/SplashActivity.java
@@ -1,17 +1,42 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class SplashActivity extends Activity {
+    private static int SPLASH_DURATION = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.i("RUN", "run a mile");
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_DURATION);
+
     }
 
     @Override
